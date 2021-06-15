83bb772e82bfcfa1bb9189efa97482fabc1eca65
a-scollin <andrewscollin@hotmail.co.uk>
2021-06-15 12:20:57 +0100

    Added some simple preprocessing to improve the OCR's performance before starting on NLP

--- OCR_helper.py
+++ OCR_helper.py
@@ -4,19 +4,21 @@ import pytesseract
 import argparse
 import cv2
 import os
+import numpy as np
+
 
 # Very simple set of functions for running the OCR from PIL image,
 # will include methods for training the OCR model once verification data is made 
 
 def read_image_to_df(image):
-    text = pytesseract.image_to_data(image, output_type='data.frame')
-
+    text = pytesseract.image_to_data(OCR_pre_process_image(image), output_type='data.frame')
+    return text[text.conf != -1]
 
 def print_from_image(image):
 
     # Running basic tesseract on the test.png in TestingSnips folder
 
-    text = pytesseract.image_to_data(image, output_type='data.frame')
+    text = pytesseract.image_to_data(OCR_pre_process_image(image), output_type='data.frame')
 
     # Removing false word predictions (ie. no confidence) 
 
@@ -24,3 +26,28 @@ def print_from_image(image):
 
     print(teswords)
 
+def OCR_pre_process_image(image):
+    
+    open_cv_image = np.array(image.convert('RGB')) 
+    grayscaled = cv2.cvtColor(open_cv_image,cv2.COLOR_BGR2GRAY)
+
+    # No threshold preprocessing should be needed for the first report as it's all evenly scanned and is already black and white.
+
+    # th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+    # cv2.imwrite('./preprocessing/grayscaled.jpg', grayscaled)
+    # cv2.imwrite('./preprocessing/adaptive_threshold_processed.jpg',th)
+   
+
+    clean = cv2.fastNlMeansDenoising(grayscaled, h=50)
+    kernel = np.ones((2,2),np.uint8)
+    erosion = cv2.erode(clean,kernel,iterations = 1)
+    
+    # change values for thinning and denoising above for different volumes
+    
+    # cv2.imshow("original",grayscaled)
+    # cv2.imshow("denoised",clean)
+    # cv2.imshow("denoised + thinned",erosion)
+    # cv2.waitKey(0)
+
+    return Image.fromarray(erosion)
+   
--- controller.py
+++ controller.py
@@ -1,14 +1,15 @@
+from os import name
 import OCR_helper as OCR
 from PIL import Image
-
+from spellchecker import SpellChecker
+import pandas as pd
 
 # Convert from path reutrns a list of PIL images making it very easy to use
 from pdf2image import convert_from_path
-        
 
 def print_all_volumes():
     for i in range(3,8):
-        images = convert_from_path("./bouldercopies/" + str(i) + "_Report_14/06.pdf", 500)    
+        images = convert_from_path("./bouldercopies/" + str(i) + "_Report_14_06.pdf", 500)    
         for image in images:
             OCR.print_from_image(image)
 
@@ -17,3 +18,26 @@ def print_one_volume(number):
     for image in images:
         OCR.print_from_image(image)
 
+# The function below is a primitive spellchecker, it currently overextends to correct punctuation which should be an easy fix
+
+def attempt_spellcheck(number):
+    
+    images = convert_from_path("./bouldercopies/" + str(number) + "_Report_14_06.pdf", 500)
+    words = []    
+    for image in images:
+         for word in OCR.read_image_to_df(image)["text"]:
+             words.append(word)
+             
+    spell = SpellChecker()
+
+    # find those words that may be misspelled
+    misspelled = spell.unknown(words)
+
+    for word in misspelled:
+        # For misspellings based on puncutation and numerics 
+        for char in word :
+            if not (char.isalpha()):
+                break
+        if char.isalpha():
+            print(word)
+            print(spell.correction(word))
