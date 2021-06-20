0541b520f42495214b66a15b3c1ac40095e5e25e
a-scollin <andrewscollin@hotmail.co.uk>
2021-06-20 16:31:25 +0100

    Got the find_size function working well, needs a slight tweak in the span space it recognises around the words, aswell as needing more keywords like "x feet high" recognised

--- NLP_helper.py
+++ NLP_helper.py
@@ -52,16 +52,209 @@ def find_boulder_from_numbered_regex(match):
 # This function analyses a sentence to extract size information relating to height and width 
 
 def find_size(flair_sentence,sentence):
-    
+
     size = re.search("[0-9]+ (X|x) [0-9]+ (X|x) [0-9]+",sentence)
-    
+
+    # hopefully will just be a simple l x b x h
     if size:
     
         return size.group(0)
 
     else: 
+    # If not, then check for length and breadth keywords.. not checking specifically for height because height is mentioned alot when desribing boulder locality
+        if "breadth".casefold() in sentence.casefold() or "length".casefold() in sentence.casefold():
+            spans = flair_sentence.get_spans('pos')
+            breadth, length, height = None, None, None
+            breadth_index, length_index, height_index = None, None, None
+
+            for i in range(0,len(spans)):
         
-        return size
+                if spans[i].text.casefold() == "breadth".casefold() and not breadth:
+                    span_counter = 0    
+                    j = i
+                    k = i 
+                    while not breadth and (j > 0 or k < len(spans) - 1):           
+                        
+                        if span_counter:
+                            # print("span")
+                            span_counter += 1 
+                            # print(span_counter)
+                            if span_counter > 8:
+                                breadth = spans[span_index].text
+                                breadth_index = span_index
+                                break
+                        
+                        if j > 0:
+                            j -= 1
+                        if k < len(spans) - 1:
+                            k += 1 
+                        for label in spans[j].labels:    
+                            if "CD" in label.value:
+                                # print("CD!")
+                                # print(spans[j].text)
+                                if (j != length_index and j != height_index and spans[j].text.isnumeric()):
+                                    breadth = spans[j].text
+                                    breadth_index = j
+                                
+                                # print(length_index)
+                                # print(j)
+                                # print(breadth)
+                                if span_counter == 0:
+                                    if (j == length_index or j == height_index) and breadth is None:
+                                        # print("span on")
+                                        span_index = j
+                                        span_counter = 1 
+
+                                    
+                        if not breadth:
+                            for label in spans[k].labels:
+                                if "CD" in label.value:
+                                    if k != length_index and k != height_index and spans[k].text.isnumeric():
+                                        # print("K")
+                                        # print(span_counter)
+                                        # print(spans[k].text)
+
+                                        # print(i)
+                                        # print(j)
+                                        # print(k)
+                                        
+                                        breadth = spans[k].text 
+                                        breadth_index = k  
+
+                                    if span_counter == 0:
+                                        if (j == length_index or j == height_index) and breadth is None:
+                                            # print("span on")
+                                            span_index = j
+                                            span_counter = 1 
+    
+                
+                
+                if spans[i].text.casefold() == "length".casefold() and not length:
+                    
+                    span_counter = 0    
+                    j = i
+                    k = i 
+                    while not length and (j > 0 or k < len(spans) - 1):           
+                        
+                        if span_counter:
+                            # print("span")
+                            span_counter += 1 
+                            # print(span_counter)
+                            if span_counter > 6:
+                                length = spans[span_index].text
+                                breadth_index = span_index
+                                break
+                        
+                        if j > 0:
+                            j -= 1
+                        if k < len(spans) - 1:
+                            k += 1 
+                        for label in spans[j].labels:    
+                            if "CD" in label.value:
+                                # print("CD!")
+                                # print(spans[j].text)
+                                if (j != breadth_index and j != height_index and spans[j].text.isnumeric()):
+                                    length = spans[j].text
+                                    breadth_index = j
+                                
+                                # print(breadth_index)
+                                # print(j)
+                                # print(length)
+                                if span_counter == 0:
+                                    if (j == breadth_index or j == height_index) and length is None:
+                                        # print("span on")
+                                        span_index = j
+                                        span_counter = 1 
+
+                                    
+                        if not length:
+                            for label in spans[k].labels:
+                                if "CD" in label.value:
+                                    if k != breadth_index and k != height_index and spans[k].text.isnumeric():
+                                        # print("K")
+                                        # print(span_counter)
+                                        # print(spans[k].text)
+
+                                        # print(i)
+                                        # print(j)
+                                        # print(k)
+                                        
+                                        length = spans[k].text 
+                                        breadth_index = k  
+
+                                    if span_counter == 0:
+                                        if (j == breadth_index or j == height_index) and length is None:
+                                            # print("span on")
+                                            span_index = j
+                                            span_counter = 1 
+
+                if spans[i].text.casefold() == "height".casefold() and not height:
+                    
+                    span_counter = 0    
+                    j = i
+                    k = i 
+                    while not height and (j > 0 or k < len(spans) - 1):           
+                        
+                        if span_counter:
+                            # print("span")
+                            span_counter += 1 
+                            # print(span_counter)
+                            if span_counter > 6:
+                                height = spans[span_index].text
+                                breadth_index = span_index
+                                break
+                        
+                        if j > 0:
+                            j -= 1
+                        if k < len(spans) - 1:
+                            k += 1 
+                        for label in spans[j].labels:    
+                            if "CD" in label.value:
+                                # print("CD!")
+                                # print(spans[j].text)
+                                if (j != breadth_index and j != length_index and spans[j].text.isnumeric()):
+                                    height = spans[j].text
+                                    breadth_index = j
+                                
+                                # print(breadth_index)
+                                # print(j)
+                                # print(height)
+                                if span_counter == 0:
+                                    if (j == breadth_index or j == length_index) and height is None:
+                                        # print("span on")
+                                        span_index = j
+                                        span_counter = 1 
+
+                                    
+                        if not height:
+                            for label in spans[k].labels:
+                                if "CD" in label.value:
+                                    if k != breadth_index and k != length_index and spans[k].text.isnumeric():
+                                        # print("K")
+                                        # print(span_counter)
+                                        # print(spans[k].text)
+
+                                        # print(i)
+                                        # print(j)
+                                        # print(k)
+                                        
+                                        height = spans[k].text 
+                                        breadth_index = k  
+
+                                    if span_counter == 0:
+                                        if (j == breadth_index or j == length_index) and height is None:
+                                            # print("span on")
+                                            span_index = j
+                                            span_counter = 1 
+                                    
+            return "Length :" + str(length) + " Breadth : " + str(breadth) + " Height : " + str(height)
+        
+        return None
+                
+
+                    
+
+        
 
 # This function analyses a sentence to extract the rock type mentioned 
 
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-safewords = []
+from flair.data import Sentence
+from flair.models import MultiTagger
+from flair.models import SequenceTagger
+from flair.tokenization import SegtokSentenceSplitter
 
+tagger = MultiTagger.load(['pos','ner'])
 
-        
+flair_sentence = Sentence("The dog went to the park and ate an apple.")
 
-print(safewords)
+tagger.predict(flair_sentence)
+
+spans = flair_sentence.get_spans('pos')
+
+print(type(spans))
+print(spans[2]["labels"])
